f4d906ad43f9ccd9fcce3bfd3fdd59dafcfaa6bc
Martin <martin.evenbom@gmail.com>
2024-04-15 16:36:51 +0200

    fixed retrieve slack message logic

--- test_draftgpt.py
+++ test_draftgpt.py
@@ -1,6 +1,5 @@
 import os
 import requests
-from slackeventsapi import SlackEventAdapter
 from urllib.parse import urlparse, parse_qs
 
 slack_events_adapter = SlackEventAdapter(os.environ["SLACK_SIGNING_SECRET"], "/slack/events")
@@ -10,24 +9,8 @@ def parse_slack_message_link(link):
     query_params = parse_qs(parsed_url.query)
     return query_params.get("text", [""])[0]
 
-
-@slack_events_adapter.on("message")
-def message(event_data):
-    message = event_data["event"]
-    user_id = message.get("user")
-    text = message.get("text")
-
-    if user_id and text:
-        user_input = parse_slack_message_link(text)
-        
-        # Call gpt function with user message
-        if user_input:
-            response = draft_gpt(user_input)
-
-        # Send response back to slack
-            print("GPT response with slack:", response)
-        
-
+def retrieve_slack_message(slack_message_link):
+    return parse_slack_message_link(slack_message_link)
 
 def draft_gpt(user_input, openai_api_key=os.environ["OPENAI_API_KEY"], gpt_model=os.environ["GPT_MODEL"]):
 
@@ -91,5 +74,16 @@ def test_draft_gpt():
 
 
 if __name__ == "__main__":
-    # draft_gpt()
-    slack_events_adapter.start(port=3000)
+    slack_message_link = os.getenv("SLACK_MESSAGE_LINK")
+    if slack_message_link:
+        user_input = retrieve_slack_message(slack_message_link)
+        if user_input:
+            response = draft_gpt(user_input)
+            if response:
+                print("GPT response:", response)
+            else:
+                print("Failed to get GPT response.")
+        else:
+            print("Failed to retrieve user input from Slack message link.")
+    else:
+        print("SLACK_MESSAGE_LINK environment variable not set.")

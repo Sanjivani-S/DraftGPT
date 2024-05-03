0695f9fa970a4cdc5ee16fbde57697aef971f1ca
Martin <martin.evenbom@gmail.com>
2024-05-03 22:15:46 +0200

    fixed syntax

--- test_draftgpt.py
+++ test_draftgpt.py
@@ -105,6 +105,7 @@ def draft_gpt(user_input, openai_api_key=os.environ["OPENAI_API_KEY"], gpt_model
     print("\n Total input for chatgpt - slack input + log file input == \n")     
     print(user_input)
 
+'''
     url = "https://api.openai.com/v1/chat/completions"
 
     headers = {
@@ -122,6 +123,7 @@ def draft_gpt(user_input, openai_api_key=os.environ["OPENAI_API_KEY"], gpt_model
             },
         ],
     }
+'''
 
     response = requests.post(url, headers=headers, json=data)
     
